2556cc5eb5f7e53cac84fcd3e0e6387aed7375c6
hitesh-pathak <97353760+hitesh-pathak@users.noreply.github.com>
2024-05-30 23:04:25 +0530

    Add Router, Basic Navbar, Router Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Navbar from "./components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="flex min-h-screen flex-col justify-between"></div>;
+  return (
+    <div className="flex min-h-screen flex-col justify-between">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/CourseSeries.jsx
@@ -0,0 +1,3 @@
+export default function CourseSeries() {
+  return <></>;
+}
